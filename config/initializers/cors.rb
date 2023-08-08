db137eafc05cd558d995017037e68b4918d47c8e
Anna Samus <annapn32@gmail.com>
2023-08-08 19:03:53 +0100

    gems

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,9 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'http://52.188.64.135:5000'
-    resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options], credentials: true
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
