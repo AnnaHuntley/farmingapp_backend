f650dad27718939a0168c950b21c0a9ce977c213
Anna Samus <annapn32@gmail.com>
2023-08-09 11:48:31 +0100

    c

--- config/application.rb
+++ config/application.rb
@@ -1,14 +1,22 @@
-module FarmingApp
+require_relative "boot"
+
+require "rails/all"
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module TodoList
   class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
-
-
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins '*' 
-        resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options]
-      end
-    end
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
   end
 end
